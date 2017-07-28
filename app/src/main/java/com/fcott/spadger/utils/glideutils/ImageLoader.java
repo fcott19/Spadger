01704b8625c3b9fda6c7bd78863143c1f802b0b2
fcott19 <fulechun@icloud.com>
2017-07-28 16:31:04 +0800

    一些微小的改动

--- app/src/main/java/com/fcott/spadger/utils/glideutils/ImageLoader.java
+++ app/src/main/java/com/fcott/spadger/utils/glideutils/ImageLoader.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.FutureTarget;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.PreloadTarget;
@@ -90,7 +89,7 @@ public class ImageLoader {
         Glide.with(context)
                 .load(url)
                 .asBitmap()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                 .listener(errorListener)
                 .into(target);
     }
